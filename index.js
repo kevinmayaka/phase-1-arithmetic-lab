387a51f8642aced2606984dd4cbd6dfc57cdc560
Kevin Mayaka <kevinabuga3@gmail.com>
2024-03-18 22:24:35 +0300

    Review: Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = (num3 % num4);
+
+let max = Math.max(10, 5, 20, 15);
